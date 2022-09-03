c1222f0db91baf380edfc6d89f61e341d59b0e88
AKAzze <56426623+loveyourz0411@users.noreply.github.com>
2022-09-03 16:06:12 +0800

    Add files via upload

---
+++ Maximum_sequence.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<cstdlib>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<cmath>
+using namespace std;
+#define MAX_N 3000004
+int q[MAX_N + 5], head, tail;
+long long arr[MAX_N + 5];
+
+int main() {
+	long long n, m;
+	cin >> n >> m;
+	for (int i = 1;i <= n;i++) {
+		cin >> arr[i], arr[i] += arr[i - 1];
+	}
+	head = tail = 0;
+	q[tail++] = 0;
+	long long ans = arr[1];
+	for (int i = 0; i <= n; i++) {
+		ans = max(ans, arr[i] - arr[q[head]]);
+		while (tail - head && arr[q[tail - 1]] >= arr[i])
+			tail--;
+		q[tail++] = i;
+		if (q[head] == i - m)
+			head++;
+	}
+	cout << ans << endl;
+	return 0;
+}
+//6 4
+//1 -3 5 1 -2 3
+//7
---
+++ vector.cpp
@@ -0,0 +1,92 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+typedef struct vector
+{
+    int* data;
+    int size, length;
+}vector;
+
+vector* init(int n) {
+    vector* vec = (vector*)malloc(sizeof(vector));
+    vec->data = (int*)malloc(sizeof(int) * n);
+    vec->size = n;
+    vec->length = 0;
+    return vec;
+}
+
+int expand(vector* vec) {
+    int new_size = vec->size * 2;
+    int *p = (int *)realloc(vec->data, sizeof(int) * new_size);
+    if (p == NULL) return 0;
+    vec->size = new_size;
+    vec->data = p;
+    return 1;
+}
+
+int insert(vector* vec, int ind, int val) {
+    if (vec == NULL) return 0;
+    if (vec->length == vec->size) {
+        if (!expand(vec)) return 0;
+        printf("expand vector size to %d success\n", vec->size);
+    }
+    if (ind < 0 || ind > vec->length) return 0;
+    for (int i = vec->length;i > ind;i--) {
+        vec->data[i] = vec->data[i - 1];
+    }
+    vec->data[ind] = val;
+    vec->length += 1;
+    return 1;
+}
+
+int erase(vector* vec, int ind) {
+    if (vec == NULL) return 0;
+    if (vec->length == 0) return 0;
+    if (ind < 0 || ind >= vec->length) return 0;
+    for (int i = ind + 1;i < vec->length;i++) {
+        vec->data[i] = vec->data[i - 1];
+    }
+    vec->length -= 1;
+    return 1;
+}
+
+void clear(vector* vec) {
+    if (vec == NULL) return;
+    free(vec->data);
+    free(vec);
+    return;
+}
+
+void output(vector* vec) {
+    printf("Vector(%d)=[", vec->length);
+    for (int i = 0;i < vec->length;i++) {
+        if (i != 0) printf(", ");
+        printf("%d", vec->data[i]);
+    }
+    printf("]\n");
+    return;
+}
+
+int main() {
+#define MAX_OP 20
+    vector* vec = init(1);
+    int op, ind, val;
+    for (int i = 0; i < MAX_OP; i++) {
+        op = rand() % 2;
+        ind = rand() % (vec->length + 1);
+        val = rand() % 100;
+        switch (op) {
+        case 0: {
+            printf("insert %d at %d to vector\n", val, ind);
+            insert(vec, ind, val);
+        } break;
+        case 1: {
+            printf("erase item at %d from vector\n", ind);
+            erase(vec, ind);
+        }break;
+        }
+        output(vec);
+    }
+    return 0;
+}
